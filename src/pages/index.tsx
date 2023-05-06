f9ec4a894288f13118a3a66f40312174ca32dae7
choi1five <cwo1401@gmail.com>
2023-05-06 20:15:35 +0900

    test20

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp; 테스트요1112312312312s
+            Get started by editing&nbsp; 테스트요1112312312312s123123
             <code className={styles.code}>src/pages/index.tsx</code>
           </p>
           <div>
